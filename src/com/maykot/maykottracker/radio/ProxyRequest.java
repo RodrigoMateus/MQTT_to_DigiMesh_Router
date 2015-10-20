19116bf548a49723d62d1cdb9afe75328a2cb390
RodrigoMateus <rodrigo.m.mateus@gmail.com>
2015-10-20 17:58:50 -0200

    Novo atributo HashMap<String, String> header;

--- src/com/maykot/maykottracker/radio/ProxyRequest.java
+++ src/com/maykot/maykottracker/radio/ProxyRequest.java
@@ -1,60 +1,34 @@
 package com.maykot.maykottracker.radio;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class ProxyRequest implements Serializable {
 
 	private static final long serialVersionUID = -4707248583815599159L;
 	private String url;
-	private String verb;
-	private String contentType;
+	private HashMap<String, String> header;
 	private String idMessage;
+	private String verb;
 	private byte[] body;
 
-	public String getVerb() {
-		return verb;
-	}
+	public String getUrl() { return url; }
 
-	public void setVerb(String verb) {
-		this.verb = verb;
-	}
+	public void setUrl(String url) { this.url = url; }
 
-	public String getIdMessage() {
-		return idMessage;
-	}
+	public HashMap<String, String> getHeader() { return header; }
 
-	public void setIdMessage(String idMessage) {
-		this.idMessage = idMessage;
-	}
+	public void setHeader(HashMap<String, String> header) { this.header = header; }
+	
+	public String getIdMessage() { return idMessage; }
 
-	public String getUrl() {
-		return url;
-	}
+	public void setIdMessage(String idMessage) { this.idMessage = idMessage; }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+	public String getVerb() { return verb; }
 
-	public String getContentType() {
-		return contentType;
-	}
+	public void setVerb(String verb) { this.verb = verb; }
 
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+	public byte[] getBody() { return body; }
 
-	public byte[] getBody() {
-		return body;
-	}
-
-	public void setBody(byte[] body) {
-		this.body = body;
-	}
-
-	@Override
-	public String toString() {
-		return "ProxyRequest [idMessage=" + idMessage + ",url=" + url + ", contentType=" + contentType + ", body="
-				+ new String(getBody()) + "]";
-	}
-
+	public void setBody(byte[] body) { this.body = body; }
 }
