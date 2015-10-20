d4250370f647f818b24ff8762548edb6cb0620c9
RodrigoMateus <rodrigo.m.mateus@gmail.com>
2015-10-20 17:58:50 -0200

    Novo atributo HashMap<String, String> header;

--- src/com/maykot/maykottracker/radio/ProxyRequest.java
+++ src/com/maykot/maykottracker/radio/ProxyRequest.java
@@ -1,60 +1,46 @@
 package com.maykot.maykottracker.radio;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class ProxyRequest implements Serializable {
 
-	private static final long serialVersionUID = -4707248583815599159L;
-	private String url;
-	private String verb;
-	private String contentType;
-	private String idMessage;
-	private byte[] body;
+    private static final long serialVersionUID = -4707248583815599159L;
+    private String url;
+    private HashMap<String, String> header;
+    private String idMessage;
+    private String verb;
+    private byte[] body;
 
-	public String getVerb() {
-		return verb;
-	}
+    public String getVerb() {
+        return verb;
+    }
 
-	public void setVerb(String verb) {
-		this.verb = verb;
-	}
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
 
-	public String getIdMessage() {
-		return idMessage;
-	}
+    public String getIdMessage() {
+        return idMessage;
+    }
 
-	public void setIdMessage(String idMessage) {
-		this.idMessage = idMessage;
-	}
+    public void setIdMessage(String idMessage) {
+        this.idMessage = idMessage;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() { return url; }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) { this.url = url; }
 
-	public String getContentType() {
-		return contentType;
-	}
+    public byte[] getBody() {
+        return body;
+    }
 
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
 
-	public byte[] getBody() {
-		return body;
-	}
+    public HashMap<String, String> getHeader() { return header; }
 
-	public void setBody(byte[] body) {
-		this.body = body;
-	}
-
-	@Override
-	public String toString() {
-		return "ProxyRequest [idMessage=" + idMessage + ",url=" + url + ", contentType=" + contentType + ", body="
-				+ new String(getBody()) + "]";
-	}
-
+    public void setHeader(HashMap<String, String> header) { this.header = header; }
 }
